bf4f7cbfcda57db70efd7382e612964ad2c3d1fc
sgent10 <sgentdas11@gmail.com>
2019-04-04 17:09:23 -0400

    Changes for organization4/4/19

---
+++ Home1/src/home1chpater1/GreetingMachine.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class GreetingMachine { 
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Please enter your name and press enter"); 
+        Scanner scan = new Scanner(System.in); 
+        String name = scan.next();
+        System.out.println("Whoa, " + name + " What an interesting name card you have!"); 
+    } // close main method
+} // close class GreetingMachine
---
+++ Home1/src/home1chpater1/Greetings.java
@@ -0,0 +1,14 @@
+
+package chapter1;
+/**
+ * 
+ * @author xxrom
+ */
+
+public class Greetings {
+    
+    public static void main(String[] args){
+        System.out.println("Hello, i'm Sean");
+        System.out.println("and today is a cloudy Monday.");
+    } // close main method
+} // cloae class

5db627e3963721fbebece9150cb5d605b27cf620
Gentle <sgent10@ccac.org>
2019-05-02 12:25:09 -0400

    array updated

--- src/week12/ProgrammingBooks.java
+++ src/week12/ProgrammingBooks.java
@@ -14,8 +14,25 @@ public class ProgrammingBooks {
         books[3] = "Javascript: The Definitive Guide";
         books[4] = "Java in a Nutshell";
         
-        
-        
     } // close main
+    
+    public static void printArrayContents(String[] aarayInsideMethod){
+        
+        System.out.println("************* Extract array value and store it **********");
+        // Extract content of each array bin, then print it to the console
+        String extractedArrayValue = aarayInsideMethod[0];
+        System.out.println("Book at array at index 0: " + extractArrayValue );
+        // do this process for each bin of teh array
+        
+        extractedArrayValue = arrayIsideMethod[1];
+        System.out.println("Book at array index 1: " + extractedArrayValue );
+        extractedArrayValue = arrayIsideMethod[2];
+        System.out.println("Book at array index 2: " + extractedArrayValue );
+        extractedArrayValue = arrayIsideMethod[3];
+        System.out.println("Book at array index 3: " + extractedArrayValue );
+        extractedArrayValue = arrayIsideMethod[4];
+        System.out.println("Book at array index 4: " + extractedArrayValue );
+        
+    } // close print array
     
 } // close class
